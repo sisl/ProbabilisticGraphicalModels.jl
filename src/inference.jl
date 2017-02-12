f590fbfcb6176e233ae48531c95c5f44427d6706
Tim Wheeler <wheelert@stanford.edu>
2017-02-12 11:33:40 -0800

    moved over the common interface

---
+++ src/inference.jl
@@ -0,0 +1,45 @@
+"""
+Abstract type for probability inference
+"""
+abstract InferenceMethod
+
+@inline function _ensure_query_nodes_in_pgm_and_not_in_evidence(qs::NodeNames, nodenames::NodeNames, ev::Assignment)
+    isempty(qs) && return
+
+    q = first(qs)
+    (q in nodenames) || throw(ArgumentError("Query $q is not in the probabilistic graphical model"))
+    haskey(ev, q) && throw(ArgumentError("Query $q is part of the evidence"))
+
+    return _ensure_query_nodes_in_pgm_and_not_in_evidence(qs[2:end], nodenames, ev)
+end
+
+"""
+Type for capturing the inference state
+"""
+immutable InferenceState{PGM<:ProbabilisticGraphicalModel}
+    pgm::PGM
+    query::NodeNames
+    evidence::Assignment
+
+    function InferenceState(pgm::ProbabilisticGraphicalModel, query::NodeNameUnion, evidence::Assignment=Assignment())
+        query = unique(convert(NodeNames, query))
+        _ensure_query_nodes_in_pgm_and_not_in_evidence(query, names(pgm), evidence)
+
+        return new(pgm, query, evidence)
+    end
+end
+
+function Base.show(io::IO, inf::InferenceState)
+    println(io, "Query: $(inf.query)")
+    println(io, "Evidence:")
+    for (k, v) in inf.evidence
+        println(io, "  $k => $v")
+    end
+end
+
+"""
+    infer(InferenceMethod, InferenceState)
+Infer p(query|evidence)
+"""
+infer(im::InferenceMethod, inf::InferenceState) = error("infer not implemented for $(typeof(im)) and $(typeof(inf))")
+infer(im::InferenceMethod, pgm::ProbabilisticGraphicalModel, query::NodeNameUnion; evidence::Assignment=Assignment()) = infer(im, InferenceState(bn, query, evidence))
